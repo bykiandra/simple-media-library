053d48b92d2455c5eb638ade7ad1f75ae31e68d8
Kiandra Ginevra <kiandrajayne@gmail.com>
2020-11-13 11:37:04 +1100

    Added stylesheet and necessary npm packages

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <h1>Simple Media Library</h1>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
